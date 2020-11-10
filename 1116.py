4e24402d6ba6ec6060e2ad1379c58419c26f32fd
brnamaia <74262856+brnamaia@users.noreply.github.com>
2020-11-10 15:18:51 -0300

    Create 1116.py

---
+++ 1116.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(1, n+1):
+    x, y = map(int,input().split())
+    if y != 0:
+        divi = x/y
+        print("{}".format (divi))
+    elif y == 0:
+        print("divisão impossível")
+    
